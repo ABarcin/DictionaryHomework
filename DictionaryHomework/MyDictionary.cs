3b7b6b71e0742de45a8e59a9354daa035313ee7c
ABarcin <74905523+ABarcin@users.noreply.github.com>
2021-01-21 23:13:14 +0300

    Add project files.

---
+++ DictionaryHomework/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryHomework
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempKeys;
+        TValue[] tempValues;
+        public MyDictionary()
+                       
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+            keys[tempKeys.Length] = key;
+            values[tempValues.Length] = value;
+            Console.WriteLine(key+" numaraya sahip "+value+" eklendi");
+        }
+
+    }
+}
---
+++ DictionaryHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, string> Ogrenci = new Dictionary<int, string>();
+            //Ogrenci.Add(10,"Ahmet");
+            //Console.WriteLine(Ogrenci[10]); // yazdırmak için sadece ögrenci numarası giriliyor ama ödevde istenen sadece add komutu o yüzden sadece add komutu yazıldı 
+            MyDictionary<int,string> isimler = new MyDictionary<int, string>();
+            isimler.Add(10,"Ahmet");
+            isimler.Add(20,"Engin");
+            isimler.Add(10,"Büşra");
+            
+        }
+    }
+}
